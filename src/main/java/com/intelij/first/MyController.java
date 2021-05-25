ae24cc2f5d18f5d4a05031416e4355c110ca6cef
Anuj <anuj.gupta@sugaldamani.com>
2021-05-25 23:58:02 +0530

    add controller

---
+++ src/main/java/com/intelij/first/MyController.java
@@ -0,0 +1,16 @@
+package com.intelij.first;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class MyController {
+    @GetMapping("/")
+    public List<User> home()
+    {
+        return Arrays.asList(new User[]{new User("Anuj","Gupta"),new User("Sumeet","Rana")});
+    }
+}
